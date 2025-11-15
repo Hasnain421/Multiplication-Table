66ba7a326219b0ec690e91844ce9fd44fa427e6e
Hasnain Abas <ha42479992@gmail.com>
2025-11-15 00:59:34 -0800

    Add files via upload

---
+++ calendar_program.py
@@ -0,0 +1,25 @@
+# import calendar
+
+# yy = 2025
+# mm = 11
+
+# print(calendar.month(yy,mm))
+
+
+# a = int(input("enter ther first number"))
+# b = int(input("enster the second number"))
+# op = input("select the operator (+,-,*,/)")
+
+# if op == '+':
+#     print(a+b)
+
+# elif op == '-':
+#     print(a-b)
+
+# elif op == '*':
+#     print(a*b)
+# elif op == '/':
+#     print(a/b)
+# else:
+#     print("invalid operator")
+
